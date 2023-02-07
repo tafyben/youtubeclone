066785735f67cdf7c9305d69168eb9d649c539d4
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-02-07 19:47:13 +0200

    sidebar bar closing styling

--- js/style.js
+++ js/style.js
@@ -2,5 +2,5 @@ var menuIcon =  document.querySelector(".menu-icon");
 var sidebar =  document.querySelector(".sidebar");
 
 menuIcon.onclick = function () {
-    sidebar.classList.toggle(".small-sidebar");
+    sidebar.classList.toggle("small-sidebar");
 }
